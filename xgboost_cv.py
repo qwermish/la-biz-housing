50f0787cda0e981396359436e9a78b71b5e77e26
Leif <didine@gmail.com>
2017-05-11 21:21:08 -0700

    Added importance graphics

--- xgboost_cv.py
+++ xgboost_cv.py
@@ -4,6 +4,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import log_loss
 from sklearn.model_selection import GridSearchCV
 import xgboost as xgb
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('all_biz_cult.csv')
 
@@ -24,8 +25,6 @@ with open('business_types.txt') as f:
                                           
 feats.append('ind_art_count') # of indep artists within 0.5 miles
 feats.append('cult_count')
-feats.append('Zip Code')
-feats.append('Floor Area-L.A. Zoning Code Definition')
 
 X = df[feats]
 Y = df['discrete_val']
@@ -73,9 +72,20 @@ final_gb = xgb.train(our_params, xgdmat, num_boost_round = 1461)
 importances = final_gb.get_fscore()
 print importances
 
-importance_frame = pd.DataFrame({'Importance': list(importances.values()), 'Feature': list(importances.keys())})
-importance_frame.sort_values(by = 'Importance', inplace = True)
-importance_frame.plot(kind = 'barh', x = 'Feature', figsize = (8,8), color = 'orange')
+plt.bar(range(5), sorted(importances.values())[:5], align='center')
+plt.xticks(range(5), sorted(importances, key=importances.get)[:5], rotation=90)
+plt.xlabel('Importances of features w.r.t. housing price')
+plt.title('Top 5 least important businesses that affect nearby housing prices')
+plt.tight_layout()
+plt.savefig('5_;st_imp_biz.png')
+
+plt.bar(range(5), sorted(importances.values())[-5:], align='center')
+plt.xticks(range(5), [elem[:24] for elem in sorted(importances, key=importances.get)[-5:]], rotation=90)
+plt.xlabel('Importances of features w.r.t. housing price')
+plt.title('Top 5 most important businesses that affect nearby housing prices')
+plt.tight_layout()
+plt.savefig('5_most_imp_biz.png')
+
                 
 testdmat = xgb.DMatrix(X_val)
 Y_test = final_gb.predict(testdmat)
